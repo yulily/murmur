846ef1415a8ba0c13fd4a496d57e5abe3eb900db
shiri <irihsmoc@gmail.com>
2024-11-17 12:13:38 +0900

    update: design

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'murmur';
+export const SITE_TITLE = 'iris';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
